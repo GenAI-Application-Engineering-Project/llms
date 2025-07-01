c4ce948356fc0ba66a7e6218c4f17a2b2a8ef50a
chlovec <alfred.okoronkwo@gmail.com>
2025-07-01 03:48:04 -0700

    added websummarizer notebook

--- webpage_summarizer/main.py
+++ webpage_summarizer/main.py
@@ -1,34 +1,8 @@
-import os
-from dotenv import load_dotenv
 from scraper.scraper import SoupScraper
+from utils.utils import load_env_variables
 from webpage_summarizer.summarizer import (
     OllamaSummarizer, OpenAISummarizer, Summarizer, Website
 )
-
-
-def get_api_key():
-    load_dotenv(override=True)
-    api_key = os.getenv('OPENAI_API_KEY')
-
-    if not api_key:
-        print(
-            "No API key was found - please head over to the troubleshooting \
-                notebook in this folder to identify & fix!"
-        )
-    elif not api_key.startswith("sk-proj-"):
-        print(
-            "An API key was found, but it doesn't start sk-proj-; please \
-                check you're using the right key - see troubleshooting \
-                    notebook"
-        )
-    elif api_key.strip() != api_key:
-        print(
-            "An API key was found, but it looks like it might have space or \
-                tab characters at the start or end - please remove them - see \
-                    troubleshooting notebook"
-        )
-    else:
-        print("API key found and looks good so far!")
 
 
 def create_summary(summarizer: Summarizer, model: str) -> None:
@@ -39,7 +13,7 @@ def create_summary(summarizer: Summarizer, model: str) -> None:
 
 
 def main():
-    get_api_key()
+    load_env_variables()
 
     url = "https://edwarddonner.com"
     scraper = SoupScraper(url)
